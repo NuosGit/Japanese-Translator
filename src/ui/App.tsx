89762ce0a6fbab156e9fccc17fc6016dc25051fb
NuosGit <aquasoun@gmail.com>
2025-11-02 14:55:55 +0700

    Rebuilt entire project structure.

---
+++ src/ui/App.tsx
@@ -0,0 +1,21 @@
+/*import { useState } from 'react'
+*/
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+            <h1 className="Search">Translate Japanese By ???</h1>
+
+            <div className="controls">
+                <button className="btn Text"></button>
+                <button className="btn File"></button>
+            </div>
+        </div>
+        <button className="btn Exit"></button>
+    </>
+  )
+}
+
+export default App
